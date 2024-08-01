52337d1754bb3f769c80cce6f006f5118989d1e1
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-02 03:13:16 +0700

    fix connect

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
-const sequelize = new Sequelize('chat_app', 'root', '1234', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.DB_HOST
 });
 
 module.exports = sequelize;
