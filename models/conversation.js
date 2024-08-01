6eb28bbce96593c0dd7b539e0f98aafb57d5033c
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-02 03:03:04 +0700

    define conversation model

---
+++ models/conversation.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Conversation = sequelize.define('conversation', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: false
+});
+
+module.exports = Conversation;
