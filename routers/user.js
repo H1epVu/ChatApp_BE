fff4f5616788a70b865d1cc8d7429b42408487f2
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-02 03:05:59 +0700

    re-up user router

--- routers/user.js
+++ routers/user.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/user')
 
 const router = express.Router();
 
-router.post('/register', userController.createUser)
-router.get('/login', userController.login)
+router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router;
