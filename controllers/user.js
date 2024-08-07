af025de2a85f0d9fca0095fa5236e7c898846fa9
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-07 15:40:39 +0700

    find user by id func

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,3 @@
-const sequelize = require('../db');
 const User = require('../models/user');
 
 exports.register = async (req, res) => {
@@ -23,18 +22,42 @@ exports.login = async (req, res) => {
 
     try {
         const user = await User.findOne({ where: { email } });
-        
+
         if (!user) {
             return res.status(401).json({ message: 'Người dùng không tồn tại!' });
         }
 
         if (password != user.password) {
-            return res.status(401).json({message: 'Sai mật khẩu!'})
+            return res.status(401).json({ message: 'Sai mật khẩu!' })
         }
 
         return res.status(200).json({ message: 'ok', user });
     } catch (error) {
-        console.error(error);       
+        console.error(error);
         return res.status(500).json({ message: 'Đã xảy ra lỗi' });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        return res.status(200).json({ message: 'ok', users });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Đã xảy ra lỗi' });
+    }
+}
+
+exports.getUserById = async (req, res) => {
+    const { userId } = req.params
+    try {
+        const user = await User.findByPk(userId)
+        if (user) {
+            return res.status(200).json(user);
+        }
+        return res.status(404).json({ message: 'Người dùng không tìm thấy' });
+    } catch (error) {
+        console.error('Lỗi khi tìm người dùng:', error);
+        return res.status(500).json({ message: 'Đã xảy ra lỗi' });
+    }
+}
