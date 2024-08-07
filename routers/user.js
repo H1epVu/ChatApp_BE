646617a26bd50c41bd29152c0b9859a0e8f64313
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-07 15:45:03 +0700

    add find userById router

--- routers/user.js
+++ routers/user.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.get('/:userId', userController.getUserById)
+router.get('/', userController.getAllUsers)
 
 module.exports = router;
